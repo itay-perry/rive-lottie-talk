d50802f6340836663155486e0ad6347c1ec7f698
itay-perry <itay@groundcover.com>
2025-05-05 20:45:04 +0300

    many controls added - playing a segment is still missing

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# rive-lottie-talk
+# rive-lottie-talk 🎃
+
+How to use?
+
+Start `Cursor` or `VSCode`
+
+Install the extension Live Server and just click **Go Live** in your editor (bottom command bar)
+
+---------------------------------------
+
+Why is this extension needed? 
+
+🌼 Modern browsers restrict `file://` protocol from loading local resources for security reasons..
+The Lottie animation tries to fetch the JSON file using XMLHttpRequest/Fetch, which is blocked under `file://` :) 🌼
---
+++ main.js
@@ -0,0 +1,96 @@
+const animation = lottie.loadAnimation({
+  container: document.getElementById("lottie"), // the DOM element
+  renderer: "svg", // or 'canvas', 'html'
+  path: "./LoadingSuccessError.json", 
+});
+
+
+
+// 🌼 Controls built to play with it in Vanilla JS 🌼
+const speedInput = document.getElementById("speed");
+const speedValue = document.getElementById("speedValue");
+const frameDisplay = document.getElementById("currentFrame");
+const loopBtn = document.getElementById("loopBtn");
+const animationProgressBar = document.getElementById("animationProgressBar");
+const playPauseBtn = document.getElementById("playPauseBtn");
+const totalFrames = document.getElementById("totalFrames");
+
+function resetPlayer() {
+  animationProgressBar.value = 0;
+  frameDisplay.textContent = 0;
+  playPauseBtn.textContent = "▶️";
+  animation.goToAndStop(0, true);
+}
+
+function init() {
+  resetPlayer();
+
+  // loop
+  animation.loop = false;
+  loopBtn.textContent = "Loop off";
+
+  const initialSpeed = 0.5;
+  animation.setSpeed(initialSpeed);
+  speedValue.textContent = initialSpeed;
+}
+
+init();
+
+speedInput.addEventListener("input", () => {
+  const speed = parseFloat(speedInput.value);
+  animation.setSpeed(speed);
+  speedValue.textContent = speed.toFixed(1);
+});
+
+animation.addEventListener("enterFrame", function (e) {
+  // frameDisplay.textContent = Math.floor(e.currentTime);
+  frameDisplay.textContent = e.currentTime.toFixed(1);
+});
+
+loopBtn.addEventListener("click", () => {
+  animation.loop = !animation.loop;
+  loopBtn.textContent = `Loop ${animation.loop ? "🔁" : "off"}`;
+});
+
+let isScrubbing = false;
+
+animation.addEventListener("enterFrame", (e) => {
+  if (isScrubbing) return; // don’t overwrite while dragging
+  console.log(e.currentTime);
+  const progressPercent = Math.round(
+    (e.currentTime / animation.totalFrames) * 100
+  );
+  console.log(progressPercent);
+  animationProgressBar.value = progressPercent.toFixed(1);
+});
+
+animation.addEventListener("complete", () => {
+  resetPlayer();
+  console.log("Animation completed");
+});
+
+animationProgressBar.addEventListener("input", () => {
+  isScrubbing = true;
+  const percent = parseFloat(animationProgressBar.value) / 100;
+  const targetFrame = percent * animation.totalFrames;
+  animation.goToAndStop(targetFrame, true);
+});
+
+animationProgressBar.addEventListener("change", () => {
+  isScrubbing = false;
+});
+
+animation.addEventListener("DOMLoaded", () => {
+  console.log("Total frames:", animation.totalFrames);
+  totalFrames.textContent = animation.totalFrames;
+});
+
+playPauseBtn.addEventListener("click", () => {
+  if (animation.isPaused || animation.isStopped) {
+    animation.play();
+    playPauseBtn.textContent = "⏸️";
+  } else {
+    animation.pause();
+    playPauseBtn.textContent = "▶️";
+  }
+});
